802b6c39455289b598cfb1663e8ec0dfe10deffb
isadorabello <isadora.bello@gec.inatel.br>
2023-03-12 23:36:49 -0300

    security matters

--- setup/settings.py
+++ setup/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-izynmer&g$31!v%27as5a#dmaqk$w04z7%fs_v!t@pg$+3yt&n'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
