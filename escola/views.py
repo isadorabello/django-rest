ad01a00160d1499f2e2aa6ccfab53556887c8588
isadorabello <isadora.bello@gec.inatel.br>
2023-03-13 21:22:02 -0300

    making first json example

---
+++ escola/views.py
@@ -0,0 +1,8 @@
+from django.http import JsonResponse
+
+
+def alunos(request):
+    if request.method =='GET':
+        aluno = {'id': 1, 'nome': 'isadora'} #json
+    
+    return JsonResponse(aluno)
