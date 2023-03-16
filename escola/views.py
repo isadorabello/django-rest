177da0e206e9313fd52c786be783a0e2da792c29
isadorabello <isadora.bello@gec.inatel.br>
2023-03-15 21:37:35 -0300

    feat: creating API code

--- escola/views.py
+++ escola/views.py
@@ -1,8 +1,24 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
 
 
-def alunos(request):
-    if request.method =='GET':
-        aluno = {'id': 1, 'nome': 'isadora'} #json
+# def alunos(request):
+#     if request.method =='GET':
+#         aluno = {'id': 1, 'nome': 'isadora'} #json
     
-    return JsonResponse(aluno)
+#     return JsonResponse(aluno)
+
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from escola.serializer import AlunoSerializer, CursoSerializer
+
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos(as)"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from escola.views import alunos
+# from escola.views import alunos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', alunos),
+    # path('alunos/', alunos),
 ]
