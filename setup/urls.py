99a09f184260c6febbad62f3767df8879e06b3b6
isadorabello <isadora.bello@gec.inatel.br>
2023-03-15 21:45:22 -0300

    feat: adjusting API code

--- setup/urls.py
+++ setup/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 # from escola.views import alunos
+from escola.views import AlunosViewSet, CursosViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
     # path('alunos/', alunos),
 ]
