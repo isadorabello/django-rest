052fde4c77dd762b63b1d556d64b3b0352ec9f7a
isadorabello <isadora.bello@gec.inatel.br>
2023-04-21 13:45:17 -0300

    feat: adding validations (not working)

--- django_rest_3/clientes/serializers.py
+++ django_rest_3/clientes/serializers.py
@@ -1,28 +1,25 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
     
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError("O CPF deve ter 11 dígitos")
-        return cpf
-    
-    def validate_nome(self, nome):
-        if not nome.isalpha():
-            raise serializers.ValidationError("Não inclua números neste campo")
-        return nome
-    
-    def validate_rg(self, rg):
-        if len(rg) != 9:
-            raise serializers.ValidationError("O RG deve ter 9 dígitos")
-        return rg
-    
-    def validate_celular(self, celular):
-        if len(celular) < 11:
-            raise serializers.ValidationError("O celular deve informar o DDD e o 9 padrão")
-        return celular
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':"O CPF deve ter 11 dígitos"})
+        
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':"Não inclua números neste campo"})
+        
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':"O RG deve ter 9 dígitos"})
+        
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':"O celular deve informar o DDD e o 9 padrão"})
+        
+        return data
+
     
---
+++ django_rest_3/clientes/validators.py
@@ -0,0 +1,11 @@
+def cpf_valido(cpf):
+    return len(cpf) == 11
+    
+def nome_valido(nome):
+    return nome.isalpha()
+    
+def rg_valido(rg):
+    return len(rg) == 9
+    
+def celular_valido(celular):
+    return len(celular) < 11
