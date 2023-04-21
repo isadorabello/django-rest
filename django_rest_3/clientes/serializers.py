af625612a7a065a127a71ce0d0e8c4bc9155ce5d
isadorabello <isadora.bello@gec.inatel.br>
2023-04-21 13:09:42 -0300

    feat: adding validations

--- django_rest_3/clientes/serializers.py
+++ django_rest_3/clientes/serializers.py
@@ -6,3 +6,8 @@ class ClienteSerializer(serializers.ModelSerializer):
         model = Cliente
         fields = '__all__'
     
+    def validate_cpf(self, cpf):
+        if len(cpf) != 11:
+            raise serializers.ValidationError("O CPF deve ter 11 dígitos")
+        return cpf
+    
